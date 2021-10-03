955424dd68f2f16db13461adfcd52db8f7d640e8
mynternet <57996208+mynternet@users.noreply.github.com>
2021-10-03 16:58:57 -0500

    Add files via upload

---
+++ src/components/BuscarProductoForm.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { Button, Form, FormGroup, Label, Input } from "reactstrap";
+import { Link } from 'react-router-dom';
+
+const BuscarProductoForm = () => {
+  return (
+    <div>
+      <section>
+        <ui className="OpcionesProductos">
+          <li>
+            <div style={{ margin: "30px" }}>
+              {" "}
+              <Link to='/listarproductos'>
+              <Button color="primary">Ver lista de productos</Button>
+              </Link>
+            </div>
+          </li>
+        </ui>
+      </section>
+      <Form className="w-75">
+        <FormGroup className="mb-3">
+          <h2 className="TituloFormProductos"> Buscar producto </h2>
+          <Label for="codigo">Busqueda por código</Label>
+          <Input
+            type="number"
+            name="codigo"
+            id="codigo"
+            placeholder="Inserte Código"
+          />
+        </FormGroup>
+
+        <FormGroup className="mb-3">
+          <Label for="descripcion">Busqueda por descripción</Label>
+          <Input
+            type="text"
+            name="descripcion"
+            id="descripcion"
+            placeholder="Inserte Descripción"
+          />
+        </FormGroup>
+
+        <div
+          style={{ display: "flex", width: "100%", justifyContent: "flex-end" }}
+        >
+          <Button color="primary">Buscar</Button>
+        </div>
+      </Form>
+    </div>
+  );
+};
+
+export default BuscarProductoForm;
